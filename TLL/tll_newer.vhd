0521754022cfb31a1ccaa7161b0688e1b60536b9
ydong <y.dong@outlook.com>
2026-01-14 10:15:40 +0800

    tll divided into multi-processes

--- TLL/tll_newer.vhd
+++ TLL/tll_newer.vhd
@@ -104,6 +104,7 @@ begin
 	process(sys_clk)
 	begin
 		if rising_edge(sys_clk) then
+--			samp_vld_d0 <= samp_vld;
 			if samp_vld = '1' then 
 				samp_i_q(24 downto 16) <= samp_i(8 downto 0);
 				samp_q_q(24 downto 16) <= samp_q(8 downto 0);
@@ -115,159 +116,198 @@ begin
 		end if;
 	end process; 
 
+	-- linear interpolator
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			if samp_vld_d(0) = '1' then
+				xI <= (ONE - mu) * signed(samp_i_d) + mu * signed(samp_i_q);
+				yI <= (ONE - mu) * signed(samp_q_d) + mu * signed(samp_q_q);
+			end if;
+		end if;
+	end process;
+
+	-- truncation, Q17.32 -> Q8.16
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			if samp_vld_d(1) = '1' then
+				if xI(49 downto 40) = "0000000000" or xI(49 downto 40) = "1111111111" then
+					xI_t(24 downto 16) <= xI(40 downto 32);
+				elsif xI(49) = '0' then
+					xI_t(24 downto 16) <= "011111111";
+				elsif xI(49) = '1' then
+					xI_t(24 downto 16) <= "100000001";
+				end if;
+				if yI(49 downto 40) = "0000000000" or yI(49 downto 40) = "1111111111" then
+					yI_t(24 downto 16) <= yI(40 downto 32);
+				elsif yI(49) = '0' then
+					yI_t(24 downto 16) <= "011111111";
+				elsif yI(49) = '1' then
+					yI_t(24 downto 16) <= "100000001";
+				end if;
+				xI_t(15 downto 0) <= xI(31 downto 16);
+				yI_t(15 downto 0) <= yI(31 downto 16);
+			end if;
+		end if;
+	end process;
+
+	-- delay interpolation samples 
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			if samp_vld_d(2) = '1' then
+				TED_buff_x(0) <= xI_t;
+				TED_buff_x(1) <= TED_buff_x(0);
+				TED_buff_y(0) <= yI_t;
+				TED_buff_y(1) <= TED_buff_y(0);
+			end if;
+		end if;
+	end process;
+
+	-- and update symbols and TED
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			if (samp_vld_d(2) = '1') and (underflow = '1') then
+				err <= TED_buff_x(0)*(TED_buff_x(1) - xI_t) + TED_buff_y(0)*(TED_buff_y(1) - yI_t);
+				en_sym <= '1';
+				sym_i	<=	std_logic_vector(xI_t);
+				sym_q	<=	std_logic_vector(yI_t);
+				err_out <=  TED_buff_x(0)*(TED_buff_x(1) - xI_t) + TED_buff_y(0)*(TED_buff_y(1) - yI_t);
+			else
+				err <= (others=>'0'); 
+				en_sym <= '0';
+			end if;
+		end if;
+	end process;
+
+	-- err: Q17.32, truncation: err_t: Q8.16
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			if samp_vld_d(3) = '1' then
+				if err(49 downto 40) = "0000000000" or err(49 downto 40) = "1111111111" then
+					err_t(24 downto 16) <= err(40 downto 32);
+				elsif err(49) = '0' then
+					err_t(24 downto 16) <= "011111111";
+				elsif err(49) = '1' then
+					err_t(24 downto 16) <= "100000001";
+				end if;
+				err_t(15 downto 0) <= err(31 downto 16);
+			end if;
+		end if;
+	end process;
+
+	-- loop filter, v: Q17.32
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			if samp_vld_d(4) = '1' then
+				vp <= K1 * err_t;
+			end if;
+			if samp_vld_d(5) = '1' then
+				vi <= vi + K2 * err_t;
+			end if;
+			if samp_vld_d(6) = '1' then
+				v <= vp + vi;
+			end if;
+		end if;
+	end process;
+
+	-- div loop gain, 2^8
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			if samp_vld_d(7) = '1' then
+				v_div <= resize(v(49 downto 8), v_div'length); -- sign-extend to full width
+			end if;
+			if samp_vld_d(8) = '1' then
+				v_div_t(24 downto 16) <= v_div(40 downto 32);
+				v_div_t(15 downto 0)  <= v_div(31 downto 16);
+			end if;
+		end if;
+	end process;
+
+	-- calculate step and prepare to update cnt, mu
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			if samp_vld_d(9) = '1' then
+				W <= HALF_ONE + v_div_t;
+			end if;
+			if samp_vld_d(10) = '1' then
+				if  W /= to_signed(0, W'length) then 
+					W_t <= W ;
+				else 
+					W_t <= MINIMAL;
+				end if;
+			end if;
+		end if;
+	end process;
+
+	-- calc cnt/W
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			if samp_vld_d(11) = '1' then
+				dividend(40 downto 0) <= std_logic_vector(cnt) & "0000000000000000";
+				divisor(24 downto 0)  <= std_logic_vector(W_t);
+			end if;
+		end if;
+	end process;
+
+	-- when div finished update mu and cnt
 	process(sys_clk)
 		variable cnt_next :signed(24 downto 0):=ONE;
 	begin
 		if rising_edge(sys_clk) then
-			if aresetn = '0' then
-				xI <= (others=>'0'); 
-				yI <= (others=>'0'); 
-				err <= (others=>'0'); 
-				en_sym <= '0';
-				sym_i  <= (others=>'0');
-				sym_q  <= (others=>'0'); 
-				vp <= (others=>'0'); 
-				vi <= (others=>'0'); 
-				v <= (others=>'0'); 
-				W <= (others=>'0'); 
-				W_t <= (others=>'0'); 
-				dividend <= (others=>'0'); 
-				divisor  <= (others=>'0'); 
-				mu  <= (others=>'0'); 
-				cnt <= (others=>'0'); 
-				mu_next <= (others=>'0'); 
-				cnt_next := (others=>'0'); 
-				underflow <= '1';
-				div_vld_d <= '0';
-			else
-				-- linear interpolator
-				if samp_vld_d(0) = '1' then
-					xI <= (ONE - mu) * signed(samp_i_d) + mu * signed(samp_i_q);
-					yI <= (ONE - mu) * signed(samp_q_d) + mu * signed(samp_q_q);
-				end if;
-				-- truncation, Q17.32 -> Q8.16
-				if samp_vld_d(1) = '1' then
-					if xI(49 downto 40) = "0000000000" or xI(49 downto 40) = "1111111111" then
-						xI_t(24 downto 16) <= xI(40 downto 32);
-					elsif xI(49) = '0' then
-						xI_t(24 downto 16) <= "011111111";
-					elsif xI(49) = '1' then
-						xI_t(24 downto 16) <= "100000001";
+			div_vld_d <= div_vld;
+			div_vld_dd <= div_vld_d;
+			if div_vld = '1' then
+				cnt_next := cnt - W;
+				if cnt_next(cnt_next'high) = '1' then
+					cnt_next := ONE + cnt_next;
+					underflow <= '1';
+					if quotient(72 downto 56) = "00000000000000000" or quotient(72 downto 56) = "11111111111111111" then
+						mu_next <= signed(quotient(56 downto 32));
+					elsif quotient(72) = '0' then
+						mu_next <= ONE; -- 1 - 1/2
+					elsif quotient(72) = '1' then 
+						mu_next <= MINIMAL;
 					end if;
-					if yI(49 downto 40) = "0000000000" or yI(49 downto 40) = "1111111111" then
-						yI_t(24 downto 16) <= yI(40 downto 32);
-					elsif yI(49) = '0' then
-						yI_t(24 downto 16) <= "011111111";
-					elsif yI(49) = '1' then
-						yI_t(24 downto 16) <= "100000001";
-					end if;
-					xI_t(15 downto 0) <= xI(31 downto 16);
-					yI_t(15 downto 0) <= yI(31 downto 16);
-				end if;
-				-- delay interpolation samples and update symbols and TED
-				if samp_vld_d(2) = '1' then
-					TED_buff_x(0) <= xI_t;
-					TED_buff_x(1) <= TED_buff_x(0);
-					TED_buff_y(0) <= yI_t;
-					TED_buff_y(1) <= TED_buff_y(0);
-				end if;
-				if (samp_vld_d(2) = '1') and (underflow = '1') then
-					err <= TED_buff_x(0)*(TED_buff_x(1) - xI_t) + TED_buff_y(0)*(TED_buff_y(1) - yI_t);
-					en_sym <= '1';
-					sym_i	<=	std_logic_vector(xI_t);
-					sym_q	<=	std_logic_vector(yI_t);
-					err_out <=  TED_buff_x(0)*(TED_buff_x(1) - xI_t) + TED_buff_y(0)*(TED_buff_y(1) - yI_t);
 				else
-					err <= (others=>'0'); 
-					en_sym <= '0';
+					underflow <= '0';
+					mu_next <= mu;
 				end if;
-				-- err: Q17.32, truncation: err_t: Q8.16
-				if samp_vld_d(3) = '1' then
-					if err(49 downto 40) = "0000000000" or err(49 downto 40) = "1111111111" then
-						err_t(24 downto 16) <= err(40 downto 32);
-					elsif err(49) = '0' then
-						err_t(24 downto 16) <= "011111111";
-					elsif err(49) = '1' then
-						err_t(24 downto 16) <= "100000001";
-					end if;
-					err_t(15 downto 0) <= err(31 downto 16);
+			end if;
+			if div_vld_d = '1' then
+				if mu_next(mu_next'high) = '1' then
+					mu <= (others=>'0'); 
+				elsif mu_next >= ONE then	
+					mu <= ONE;
+				else
+					mu <= mu_next;
 				end if;
-				-- loop filter, v: Q17.32
-				if samp_vld_d(4) = '1' then
-					vp <= K1 * err_t;
-				end if;
-				if samp_vld_d(5) = '1' then
-					vi <= vi + K2 * err_t;
-				end if;
-				if samp_vld_d(6) = '1' then
-					v <= vp + vi;
-				end if;
-				-- div loop gain, 2^8
-				if samp_vld_d(7) = '1' then
-					v_div <= resize(v(49 downto 8), v_div'length); -- sign-extend to full width
-				end if;
-				if samp_vld_d(8) = '1' then
-					v_div_t(24 downto 16) <= v_div(40 downto 32);
-					v_div_t(15 downto 0)  <= v_div(31 downto 16);
-				end if;
-				-- calculate step and prepare to update cnt, mu
-				if samp_vld_d(9) = '1' then
-					W <= HALF_ONE + v_div_t;
-				end if;
-				if samp_vld_d(10) = '1' then
-					if  W /= to_signed(0, W'length) then 
-						W_t <= W ;
-					else 
-						W_t <= MINIMAL;
-					end if;
-				end if;
-				-- calc cnt/W
-				if samp_vld_d(11) = '1' then
-					dividend(40 downto 0) <= std_logic_vector(cnt) & "0000000000000000";
-					divisor(24 downto 0)  <= std_logic_vector(W_t);
-				end if;
-				-- when div finished update mu and cnt
-				if div_vld = '1' then
-					cnt_next := cnt - W;
-					if cnt_next(cnt_next'high) = '1' then
-						cnt_next := ONE + cnt_next;
-						underflow <= '1';
-						if quotient(72 downto 56) = "00000000000000000" or quotient(72 downto 56) = "11111111111111111" then
-							mu_next <= signed(quotient(56 downto 32));
-						elsif quotient(72) = '0' then
-							mu_next <= ONE; -- 1 - 1/2
-						elsif quotient(72) = '1' then 
-							mu_next <= MINIMAL;
-						end if;
-					else
-						underflow <= '0';
-						mu_next <= mu;
-					end if;
-				end if;
-				div_vld_d <= div_vld;
-				div_vld_dd <= div_vld_d;
-				if div_vld_d = '1' then
-					if mu_next(mu_next'high) = '1' then
-						mu <= (others=>'0'); 
-					elsif mu_next >= ONE then	
-						mu <= ONE;
-					else
-						mu <= mu_next;
-					end if;
-					cnt <= cnt_next;
-					dmu <= mu_next - mu;
-				end if;
-				dmu_out_vld <= div_vld_dd;
-				if div_vld_dd = '1' then
-					if dmu > HALF_ONE then
-						dmu_out <= std_logic_vector(dmu - ONE);
-					elsif dmu < HALF_ONE_NEG then
-						dmu_out <= std_logic_vector(dmu + ONE);
-					end if;
+				cnt <= cnt_next;
+				dmu <= mu_next - mu;
+			end if;
+		end if;
+	end process;
+
+	process(sys_clk)
+	begin
+		if rising_edge(sys_clk) then
+			dmu_out_vld <= div_vld_dd;
+			if div_vld_dd = '1' then
+				if dmu > HALF_ONE then
+					dmu_out <= std_logic_vector(dmu - ONE);
+				elsif dmu < HALF_ONE_NEG then
+					dmu_out <= std_logic_vector(dmu + ONE);
 				end if;
 			end if;
 		end if;
-	end process; 
+	end process;
 
 	u_div_gen: div_gen
 	port map(
@@ -280,6 +320,160 @@ begin
 				m_axis_dout_tvalid 			=> div_vld,
 				m_axis_dout_tdata 			=> quotient
 			);
+
+--	process(sys_clk)
+--		variable cnt_next :signed(24 downto 0):=ONE;
+--	begin
+--		if rising_edge(sys_clk) then
+--			if aresetn = '0' then
+--				xI <= (others=>'0'); 
+--				yI <= (others=>'0'); 
+--				err <= (others=>'0'); 
+--				en_sym <= '0';
+--				sym_i  <= (others=>'0');
+--				sym_q  <= (others=>'0'); 
+--				vp <= (others=>'0'); 
+--				vi <= (others=>'0'); 
+--				v <= (others=>'0'); 
+--				W <= (others=>'0'); 
+--				W_t <= (others=>'0'); 
+--				dividend <= (others=>'0'); 
+--				divisor  <= (others=>'0'); 
+--				mu  <= (others=>'0'); 
+--				cnt <= (others=>'0'); 
+--				mu_next <= (others=>'0'); 
+--				cnt_next := (others=>'0'); 
+--				underflow <= '1';
+--				div_vld_d <= '0';
+--			else
+--				-- linear interpolator
+--				if samp_vld_d(0) = '1' then
+--					xI <= (ONE - mu) * signed(samp_i_d) + mu * signed(samp_i_q);
+--					yI <= (ONE - mu) * signed(samp_q_d) + mu * signed(samp_q_q);
+--				end if;
+--				-- truncation, Q17.32 -> Q8.16
+--				if samp_vld_d(1) = '1' then
+--					if xI(49 downto 40) = "0000000000" or xI(49 downto 40) = "1111111111" then
+--						xI_t(24 downto 16) <= xI(40 downto 32);
+--					elsif xI(49) = '0' then
+--						xI_t(24 downto 16) <= "011111111";
+--					elsif xI(49) = '1' then
+--						xI_t(24 downto 16) <= "100000001";
+--					end if;
+--					if yI(49 downto 40) = "0000000000" or yI(49 downto 40) = "1111111111" then
+--						yI_t(24 downto 16) <= yI(40 downto 32);
+--					elsif yI(49) = '0' then
+--						yI_t(24 downto 16) <= "011111111";
+--					elsif yI(49) = '1' then
+--						yI_t(24 downto 16) <= "100000001";
+--					end if;
+--					xI_t(15 downto 0) <= xI(31 downto 16);
+--					yI_t(15 downto 0) <= yI(31 downto 16);
+--				end if;
+--				-- delay interpolation samples and update symbols and TED
+--				if samp_vld_d(2) = '1' then
+--					TED_buff_x(0) <= xI_t;
+--					TED_buff_x(1) <= TED_buff_x(0);
+--					TED_buff_y(0) <= yI_t;
+--					TED_buff_y(1) <= TED_buff_y(0);
+--				end if;
+--				if (samp_vld_d(2) = '1') and (underflow = '1') then
+--					err <= TED_buff_x(0)*(TED_buff_x(1) - xI_t) + TED_buff_y(0)*(TED_buff_y(1) - yI_t);
+--					en_sym <= '1';
+--					sym_i	<=	std_logic_vector(xI_t);
+--					sym_q	<=	std_logic_vector(yI_t);
+--					err_out <=  TED_buff_x(0)*(TED_buff_x(1) - xI_t) + TED_buff_y(0)*(TED_buff_y(1) - yI_t);
+--				else
+--					err <= (others=>'0'); 
+--					en_sym <= '0';
+--				end if;
+--				-- err: Q17.32, truncation: err_t: Q8.16
+--				if samp_vld_d(3) = '1' then
+--					if err(49 downto 40) = "0000000000" or err(49 downto 40) = "1111111111" then
+--						err_t(24 downto 16) <= err(40 downto 32);
+--					elsif err(49) = '0' then
+--						err_t(24 downto 16) <= "011111111";
+--					elsif err(49) = '1' then
+--						err_t(24 downto 16) <= "100000001";
+--					end if;
+--					err_t(15 downto 0) <= err(31 downto 16);
+--				end if;
+--				-- loop filter, v: Q17.32
+--				if samp_vld_d(4) = '1' then
+--					vp <= K1 * err_t;
+--				end if;
+--				if samp_vld_d(5) = '1' then
+--					vi <= vi + K2 * err_t;
+--				end if;
+--				if samp_vld_d(6) = '1' then
+--					v <= vp + vi;
+--				end if;
+--				-- div loop gain, 2^8
+--				if samp_vld_d(7) = '1' then
+--					v_div <= resize(v(49 downto 8), v_div'length); -- sign-extend to full width
+--				end if;
+--				if samp_vld_d(8) = '1' then
+--					v_div_t(24 downto 16) <= v_div(40 downto 32);
+--					v_div_t(15 downto 0)  <= v_div(31 downto 16);
+--				end if;
+--				-- calculate step and prepare to update cnt, mu
+--				if samp_vld_d(9) = '1' then
+--					W <= HALF_ONE + v_div_t;
+--				end if;
+--				if samp_vld_d(10) = '1' then
+--					if  W /= to_signed(0, W'length) then 
+--						W_t <= W ;
+--					else 
+--						W_t <= MINIMAL;
+--					end if;
+--				end if;
+--				-- calc cnt/W
+--				if samp_vld_d(11) = '1' then
+--					dividend(40 downto 0) <= std_logic_vector(cnt) & "0000000000000000";
+--					divisor(24 downto 0)  <= std_logic_vector(W_t);
+--				end if;
+--				-- when div finished update mu and cnt
+--				if div_vld = '1' then
+--					cnt_next := cnt - W;
+--					if cnt_next(cnt_next'high) = '1' then
+--						cnt_next := ONE + cnt_next;
+--						underflow <= '1';
+--						if quotient(72 downto 56) = "00000000000000000" or quotient(72 downto 56) = "11111111111111111" then
+--							mu_next <= signed(quotient(56 downto 32));
+--						elsif quotient(72) = '0' then
+--							mu_next <= ONE; -- 1 - 1/2
+--						elsif quotient(72) = '1' then 
+--							mu_next <= MINIMAL;
+--						end if;
+--					else
+--						underflow <= '0';
+--						mu_next <= mu;
+--					end if;
+--				end if;
+--				div_vld_d <= div_vld;
+--				div_vld_dd <= div_vld_d;
+--				if div_vld_d = '1' then
+--					if mu_next(mu_next'high) = '1' then
+--						mu <= (others=>'0'); 
+--					elsif mu_next >= ONE then	
+--						mu <= ONE;
+--					else
+--						mu <= mu_next;
+--					end if;
+--					cnt <= cnt_next;
+--					dmu <= mu_next - mu;
+--				end if;
+--				dmu_out_vld <= div_vld_dd;
+--				if div_vld_dd = '1' then
+--					if dmu > HALF_ONE then
+--						dmu_out <= std_logic_vector(dmu - ONE);
+--					elsif dmu < HALF_ONE_NEG then
+--						dmu_out <= std_logic_vector(dmu + ONE);
+--					end if;
+--				end if;
+--			end if;
+--		end if;
+--	end process; 
 
 --	process(sys_clk)
 --	begin
