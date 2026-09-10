81f997f77408db72ffd39316f01c6c2f70d32522
dongyue <y.dong@outlook.com>
2026-09-10 23:57:49 +0800

    recent changes

--- tb_hs_dem.vhd
+++ tb_hs_dem.vhd
@@ -50,10 +50,10 @@ architecture sim of tb_tb_hs_dem is
     -- File I/O
   --  file rec_r_i0 : text open read_mode  is "D:\projects\46_high_speed_dem\sim\rd_data_i.txt";
   --  file rec_r_q0 : text open read_mode  is "D:\projects\46_high_speed_dem\sim\rd_data_q.txt";
-    file rec_r_i0 : text open read_mode  is "ddc_i0.txt";
-    file rec_r_q0 : text open read_mode  is "ddc_q0.txt";
-    file rec_r_i1 : text open read_mode  is "ddc_i0.txt";
-    file rec_r_q1 : text open read_mode  is "ddc_q0.txt";
+    file rec_r_i0 : text open read_mode  is "/home/dev/ai/paralell_gardner/data_i0.txt";
+    file rec_r_q0 : text open read_mode  is "/home/dev/ai/paralell_gardner/data_q0.txt";
+    file rec_r_i1 : text open read_mode  is "/home/dev/ai/paralell_gardner/data_i1.txt";
+    file rec_r_q1 : text open read_mode  is "/home/dev/ai/paralell_gardner/data_q1.txt";
     file rec_w_i : text open write_mode is "dem_byte.txt";
 
 begin
